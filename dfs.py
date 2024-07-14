d47c9ab6d16cbf60206aa57774fbb9ed890aabc1
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-07-14 17:34:46 +0330

    reading about DFS and python code of that

---
+++ dfs.py
@@ -0,0 +1,37 @@
+graph = {
+  'A' : ['B','G'],
+  'B' : ['C', 'D', 'E'],
+  'C' : [],
+  'D' : [],
+  'E' : ['F'],
+  'F' : [],
+  'G' : ['H'],
+  'H' : ['I'],
+  'I' : [],
+}
+
+
+def dfs(graph, node):
+    visited = set()
+    stack = []
+
+    visited.add(node)
+    stack.append(node) 
+
+    while stack:
+        s = stack.pop()
+        print(s, end = ' ')
+
+        # Reverse iterate through the edge list so results match recursive version.
+        for n in reversed(graph[s]):
+            if n not in visited:
+                
+                visited.add(n)
+                stack.append(n)
+    
+
+def main():
+    dfs(graph, 'A')
+
+
+main()
