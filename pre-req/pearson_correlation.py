750982dfba8989997c3c3bb9a037bd97f2a5432b
alireza-hashemii <alirezahashemi.dev@gmail.com>
2024-11-25 14:14:12 +0330

    univariate linear regression implemented

--- pre-req/pearson_correlation.py
+++ pre-req/pearson_correlation.py
@@ -1,1 +1,14 @@
+import matplotlib.pyplot as plt
 
+x = [2.75, 4, 3.85, 3, 4.8, 1.13, 
+    2.3, 3.7, 5, 4.05,1.5,1.8,1,3.3,
+]
+
+y = [33, 66, 60, 44, 88, 11, 30,
+    54, 99, 66.5, 25, 27, 6, 62
+]
+
+plt.scatter(x, y)
+plt.xlabel("GPA SCORE")
+plt.ylabel("APPLY RATE")
+plt.show()
